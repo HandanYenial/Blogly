5182a3a8421e2eb7ad81d64a8c9ee6f0259e6c7a
Handan Yenial <88174651+HandanYenial@users.noreply.github.com>
2022-03-05 23:26:16 -0500

    Update models.py
    Post class is added

--- models.py
+++ models.py
@@ -3,10 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-
-
-
 ICON_URL = "https://icons.iconarchive.com/icons/dakirby309/windows-8-metro/256/Folders-OS-User-No-Frame-Metro-icon.png"
 
 class User(db.Model):
@@ -26,3 +22,37 @@ class User(db.Model):
                  
 
     image_url = db.Column(db.Text, nullable=False, default= ICON_URL)
+    
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
+
+class Post(db.Model):
+    """Create posts for users"""
+
+    __tablename__ ='posts'
+
+    id = db.Column(db.Integer, 
+                   primary_key=True) 
+
+    title = db.Column(db.Text, 
+                     nullable=False)
+
+    content = db.Column(db.Text,
+                        nullable=False)
+
+    created_at = db.Column(db.DateTime,
+                           nullable = False,
+                           default = datetime.datetime.now)
+
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'),
+                        nullable = False)
+                        
+
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+        
