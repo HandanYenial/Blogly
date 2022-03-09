79cb31eaf66ace8a5132d3500de023718d90a245
Handan Yenial <88174651+HandanYenial@users.noreply.github.com>
2022-03-08 21:38:32 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 """Models for Blogly."""
+
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
 
 ICON_URL = "https://icons.iconarchive.com/icons/dakirby309/windows-8-metro/256/Folders-OS-User-No-Frame-Metro-icon.png"
 
@@ -13,12 +16,12 @@ class User(db.Model):
                    primary_key=True,
                    autoincrement=True) 
 
-    first_name = db.Column(db.Text(15), 
-                     nullable=False) 
+    first_name = db.Column(db.Text, 
+                           nullable=False) 
                      
 
-    last_name = db.Column(db.Text(15),
-                     nullable=False) 
+    last_name = db.Column(db.Text,
+                          nullable=False) 
                  
 
     image_url = db.Column(db.Text, nullable=False, default= ICON_URL)
@@ -54,5 +57,44 @@ class Post(db.Model):
         """Return nicely-formatted date."""
 
         return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
+#There should also be a model for PostTag, which joins together a Post and a Tag. 
+#It will have foreign keys for the both the post_id and tag_id. 
+#Since we don’t want the same post to be tagged to the same tag more than once, 
+#we’ll want the combination of post + tag to be unique. It also makes sense that neither 
+#the post_id nor tag_id can be null.Add relationships so you can see the .tags for a post, and the .posts for a tag.
+
+class PostTag(db.Model):
+    """Post tag joins together a post and a tag"""
+
+    __tablename__ = "posts_tags"
+    post_id = db.Column(db.Integer,
+                        db.ForeignKey('posts.id'),
+                        primary_key = True)
+
+    tag_id = db.Column(db.Integer,
+                       db.ForeignKey('tags.id'),
+                       primary_key = True)
+
+class Tag(db.Model):
+    """Tag that can be added to posts."""
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship(
+        'Post',
+        secondary="posts_tags",
+        # cascade="all,delete",
+        backref="tags",
+    )
+
+  
+
+
+    
 
         
